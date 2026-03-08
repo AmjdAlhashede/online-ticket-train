7ee1755ddb3801c8796acc1fbfe1d10e95859359
Amjd Alhashede <amjdalhashede@gmail.com>
2026-03-09 01:08:53 +0300

    feat: Configure NextAuth with a secret.

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import authConfig from "./auth.config"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+    secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET || "fallback_secret_for_academic_project_981273918273",
     session: { strategy: "jwt" },
     ...authConfig,
 })
